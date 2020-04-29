d9aaa00d422347252a177429d3dfb2b998798906
Alexandre Fukaya <afukaya@hotmail.com>
2020-04-29 14:39:41 -0300

    Added new examples

--- scanwExample.c
+++ scanwExample.c
@@ -1,18 +1,18 @@
-#include <ncurses.h>			/* ncurses.h includes stdio.h */  
+#include <ncurses.h>			                                               /* ncurses.h includes stdio.h */  
 #include <string.h> 
  
 int main()
 {
- char mesg[]="Just a string";		                                           /* message to be appeared on the screen */
+ char mesg[]="Enter a string: ";	                	                       /* message to be appeared on the screen */
+ char str[80];
  int row,col;				                                                   /* to store the number of rows and *
 					                                                            * the number of colums of the screen */
  initscr();				                                                       /* start the curses mode */
  getmaxyx(stdscr,row,col);		                                               /* get the number of rows and columns */
  mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);
-                                	                                            /* print the message at the center of the screen */
- mvprintw(row-2,0,"This screen has %d rows and %d columns\n",row,col);
- printw("Try resizing your window(if possible) and then run this program again");
- refresh();
+                     		                                                   /* print the message at the center of the screen */
+ getstr(str);
+ mvprintw(LINES - 2, 0, "You Entered: %s", str);
  getch();
  endwin();
 
