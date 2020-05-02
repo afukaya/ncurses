fd0f45a429ed86c4ed6aa638d4c6df87c464ce10
Alexandre Fukaya <afukaya@hotmail.com>
2020-05-01 22:58:28 -0300

    Work update 20200501_2258

--- attributeExample.c
+++ attributeExample.c
@@ -21,26 +21,25 @@ int main(int argc, char *argv[])
     perror("Cannot open input file");
     exit(1);
   }
-  initscr();				                                                   /* Start curses mode */
-  getmaxyx(stdscr, row, col);		                                           /* find the boundaries of the screeen */
-  while((ch = fgetc(fp)) != EOF)	                                           /* read the file till we reach the end */
+  initscr();				                                                           /* Start curses mode */
+  getmaxyx(stdscr, row, col);		                                               /* find the boundaries of the screeen */
+  while((ch = fgetc(fp)) != EOF)	                                             /* read the file till we reach the end */
   {
-    getyx(stdscr, y, x);		                                               /* get the current cursor position */
-    if(y == (row - 1))			                                               /* are we are at the end of the screen */
+    getyx(stdscr, y, x);		                                                   /* get the current cursor position */
+    if(y == (row - 1))			                                                   /* are we are at the end of the screen */
     {
       printw("<-Press Any Key->");	                                           /* tell the user to press a key */
       getch();
-      clear();				                                                   /* clear the screen */
-      move(0, 0);			                                                   /* start at the beginning of the screen */
+      clear();				                                                         /* clear the screen */
+      move(0, 0);			                                                         /* start at the beginning of the screen */
     }
 
-    if(prev == '/' && ch == '*')    	                                       /* If it is / and * then only
-                                     	                                        * switch bold on */    
+    if(prev == '/' && ch == '*')    	                                         /* If it is / and * then only  switch bold on */    
     {
-      attron(A_BOLD);			                                               /* cut bold on */
-      getyx(stdscr, y, x);		                                               /* get the current curser position */
-      move(y, x - 1);			                                               /* back up one space */
-      printw("%c%c", '/', ch); 		                                           /* The actual printing is done here */
+      attron(A_BOLD);			                                                     /* cut bold on */
+      getyx(stdscr, y, x);		                                                 /* get the current curser position */
+      move(y, x - 1);			                                                     /* back up one space */
+      printw("%c%c", '/', ch); 		                                             /* The actual printing is done here */
     }
     else {
         printw("%c", ch);
@@ -48,8 +47,7 @@ int main(int argc, char *argv[])
 
     refresh();
     if(prev == '*' && ch == '/')
-      attroff(A_BOLD);        		                                           /* Switch it off once we got *
-                                 	                                            * and then / */
+      attroff(A_BOLD);        		                                             /* Switch it off once we got * and then / */
     prev = ch;
   }
   endwin();                       	                                           /* End curses mode */
